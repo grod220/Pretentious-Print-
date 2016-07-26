39c9a13c7b4ed8fb1d7b443b173ea8371370a523
Chris DeMoll <demoll.chris@gmail.com>
2016-07-26 16:53:02 -0400

    review post put and get routes

--- server/app/routes/reviewRoutes.js
+++ server/app/routes/reviewRoutes.js
@@ -23,7 +23,36 @@ router.get('/:id', function(req,res,next) {
   })
   .catch(next);
 });
+
+// Fetch one review by ID
+router.get('/:reviewId', function(req, res, next) {
+  Review.findById({
+    where: {
+      id: req.params.reviewId
+    }
+  })
+  .then(function(review) {
+    res.send(review);
+  })
+  .catch(next)
+})
     // PUT/Update one
+
+router.put('/:reviewId', function(req, res, next) {
+  Review.update(req.body, {
+    where: {
+      id: req.params.reviewId
+    },
+    returning: true
+  })
+  .then(function(review) {
+    res.send(review[1][0].dataValues);
+  })
+})
+
     // Admin or owner: DELETE
+router.delete('/:reviewId', function(req, res, next) {
+  // to be set up after sessions
+})
 
 module.exports = router;

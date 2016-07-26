319b87e0e78c47a6db34ce714bd90629976274d1
Peter Lynn <github@fantutte.net>
2016-07-26 17:27:41 -0400

    checkpoint

--- tests/server/routes/productsRoutesTest.js
+++ tests/server/routes/productsRoutesTest.js
@@ -9,16 +9,32 @@ var supertest = require('supertest');
 
 describe('Products Route', function () {
 
+    var agent;
+    var book;
+
+    var productInfo = {
+      price: 100,
+      inventory: 10,
+      image: 'http://lorempixel.com/400/200',
+      pretentionLevel: 3,
+      title: 'a nice book',
+      author: 'James Joyce'
+    };
+
+
     var app, Product;
 
     beforeEach('Sync DB', function () {
         return db.sync({ force: true });
     });
 
-    beforeEach('Create app', function () {
+    beforeEach('Create app and client agent', function () {
         app = require('../../../server/app')(db);
         Product = db.model('product');
+        agent = supertest.agent(app);
     });
+
+
 
   // describe('Unauthenticated request', function () {
 
@@ -36,30 +52,31 @@ describe('Products Route', function () {
 
   // });
 
+  describe('Create new Products', function () {
+
+   it('should post a book to the database and return us the expected thing', function (done) {
+      agent.post('/api/products/')
+      .send(productInfo)
+      .expect(201)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.body).to.be.an('object');
+        expect(res.body.title).to.equal(productInfo.title);
+        expect(res.body.title).to.equal(productInfo.title);
+        done();
+      })
+    });
+
+  });
+
   describe('Existing Products', function () {
 
-    var agent;
-    var book;
-
-    var productInfo = {
-      price: 100,
-      inventory: 10,
-      image: 'http://lorempixel.com/400/200',
-      pretentionLevel: 3,
-      title: 'a nice book',
-      author: 'James Joyce'
-    };
 
     beforeEach('Create a product', function (done) {
       return Product.create(productInfo).then(function (product) {
                 book = product;
                 done();
             }).catch(done);
-    });
-
-    beforeEach('Create a client agent', function (done) {
-      agent = supertest.agent(app);
-      done();
     });
 
     it('should get all, with 200 response and with an array as the body and the correct title', function (done) {
@@ -84,6 +101,15 @@ describe('Products Route', function () {
       });
     });
 
+    it('should get one book with 200 response and a book as the body and the correct title', function (done) {
+      agent.get('/api/products/' + book.id)
+      .expect(200).end(function (err, response) {
+        if (err) return done(err);
+        expect(response.body).to.be.an('object');
+        expect(response.body.title).to.equal(book.title);
+        done();
+      });
+    });
   });
 
 });

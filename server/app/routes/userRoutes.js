bd759e81a19359581c5fac5f707c27d8e9a7e476
Gabe Rodriguez <grod220@gmail.com>
2016-07-26 17:29:44 -0400

    in progress

--- server/app/routes/userRoutes.js
+++ server/app/routes/userRoutes.js
@@ -5,5 +5,30 @@ let db = require('../../db');
 let User = db.model('user');
 
 
+// delete a user *ADMIN*
+// set a current user to admin *ADMIN*
+// fetch all users *ADMIN*
+// update user info
+
+// fetch a single user by ID
+router.get('/:id', function(req,res,next) {
+  User.findById({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(function(result) {
+    res.send(result);
+  })
+  .catch(next);
+});
+
+// create a new user
+router.post('/', function(req,res,next) {
+  User.create(req.body)
+  .then(function(result) {
+    res.send(result);
+  });
+});
 
 module.exports = router;

56a34f0dd0a9b24d73a0cff1b13e15baa339ca8c
Peter Lynn <github@fantutte.net>
2016-07-28 13:00:52 -0400

    checkpoint

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -18,7 +18,8 @@ app.controller('testController', function($scope, $http) {
     console.log('did this work?');
     $http.post('/api/lineItems/1', obj)
     .then(function(result) {
-      alert('You just bought' + result.quantity + ' of ' + result.title);
+      console.log("The result was");
+      console.log(result);
     })
     .catch(function(error) {
       console.error(error);
